bcb9a9d6cfaa7a04334c6ed7a746b92ab4f68a5f
Sunaga0709 <tsunaga0709@gmail.com>
2022-01-05 10:17:02 +0900

    week start sunday

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,13 @@
 class ToppagesController < ApplicationController
+  before_action :set_beginning_of_week
+
   def index #ログインしていたら予定を表示
     @schedules = current_user.schedules.all if logged_in?
+  end
+  
+  private
+  
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
   end
 end
